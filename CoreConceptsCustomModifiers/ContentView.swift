88a05353947c109e526f4026b3ee9d74d3380198
Ramill Ibragimov <ramill_ibragimov@icloud.com>
2020-07-30 11:45:59 +0300

    same as before, but cleaner

--- CoreConceptsCustomModifiers/ContentView.swift
+++ CoreConceptsCustomModifiers/ContentView.swift
@@ -13,10 +13,18 @@ private let gradient = LinearGradient([Color.purple, Color.pink], to: .trailing)
 struct CustomCircleModifier: View {
     var body: some View {
         Circle()
-            .fill(gradient)
-            .overlay(Circle().stroke(Color.blue, lineWidth: 2))
+            //.fill(gradient)
+            //.overlay(Circle().stroke(Color.blue, lineWidth: 2))
+            .strokeWithFill(Color.blue, fill: gradient)
             .shadow(5)
             .frame(100)
+    }
+}
+
+private extension Shape {
+    func strokeWithFill<SS: ShapeStyle, FS: ShapeStyle>(_ strokeContents: SS, lineWidth: CGFloat = 2, fill: FS) -> some View {
+        self.fill(gradient)
+            .overlay(self.stroke(strokeContents, lineWidth: lineWidth))
     }
 }
 
