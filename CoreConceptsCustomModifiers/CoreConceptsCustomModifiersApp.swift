bdddc26483286297d5ee53a3c7f008eeedfd1368
Ramill Ibragimov <ramill_ibragimov@icloud.com>
2020-07-30 11:34:22 +0300

    Initial Commit

---
+++ CoreConceptsCustomModifiers/CoreConceptsCustomModifiersApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoreConceptsCustomModifiersApp.swift
+//  CoreConceptsCustomModifiers
+//
+//  Created by ramil on 30.07.2020.
+//
+
+import SwiftUI
+
+@main
+struct CoreConceptsCustomModifiersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
